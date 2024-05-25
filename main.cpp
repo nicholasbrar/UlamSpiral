bbe4ce21c1cf833f536ad6e51e52d90750cbc70c
Nicholas Brar <nicholas.brar@gmail.com>
2024-05-25 19:40:03 -0400

    NB-PrimeC
    none

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ bool isPrime(int num){
   if(num <= 1) return false;
   if(num <= 3) return true; 
   if(num % 2 == 0 || num % 3 == 0) return false;
-  for (int i = 5; i * i <= num; i += 6)
+  for (int i = 5; i * i <= num; i += 6)  // prime check
       if (num % i == 0 || num % (i + 2) == 0)
           return false;
   return true;

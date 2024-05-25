87801a1b57aabb17b7f262a3c5fe5c0b8818d884
nicholasbrar <153023837+nicholasbrar@users.noreply.github.com>
2024-05-25 19:15:45 -0400

    Create main.cpp
    First commit

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <iostream> 
+#include <vector>
+#include <cmath>
+#include <stdlib.h> 
+
+bool isPrime(int num){
+  if(num <= 1) return false;
+  if(num <= 3) return true; 
+  if(num % 2 == 0 || num % 3 == 0) return false;
+  for (int i = 5; i * i <= num; i += 6)
+      if (num % i == 0 || num % (i + 2) == 0)
+          return false;
+  return true;
+}
+
+void printSpiral(int num){
+  int size = sqrt(num);
+  std::vector<std::vector<int>> spiral(size, std::vector<int>(size, 0));
+
+  int x = size / 2, y = size / 2; 
+
+  if(num % 2 == 0){ 
+    x -= 1;
+    y -= 1;
+  }
+
+  int dirs[4][2] = {{0,1}, {-1,0}, {0,-1}, {1,0}}; // Right, Up, Left, Down
+  int dir = 0;  // Start direction is right
+  int steps = 1; // Steps to take in current direction
+
+  int val = 1; // Initial value to fill in spiral, Increments 
+  while (val <= num) {
+      for (int i = 0; i < steps; ++i) { // Move steps in current direction
+          if (x >= 0 && x < size && y >= 0 && y < size) {
+              spiral[x][y] = val++;
+              if (val > num) break; // Stop if we've placed all numbers
+          }
+          // Move to next cell in the current direction
+          x += dirs[dir][0];
+          y += dirs[dir][1];
+      }
+
+      dir = (dir + 1) % 4; // Change direction
+
+      // Increase step count every two direction changes (one full cycle: right-down-left-up)
+      if (dir == 0 || dir == 2) steps++;
+  }
+
+
+
+  for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+          if(isPrime(spiral[i][j])){
+            std::cout << "\033[31m*\033[0m" << " ";
+          }
+          else{
+            std::cout << "\033[34m#\033[0m" << " ";
+          }
+      }
+      std::cout << std::endl;
+  }
+}
+
+bool isPerfectSquare(int num){
+  return(sqrt(num) * sqrt(num) == num);
+}
+
+
+int main(){
+  int n;
+  std::cin >> n; 
+
+  if(isPerfectSquare(n)){
+    printSpiral(n);
+  }
+  else{
+    std::cout << "Invalid Input, Must be a perfect square" << std::endl;
+  }
+  
+  return 0;
+}
